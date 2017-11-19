5071c25262edb7e4236ba4d28f298e4eedc9b8c8
Gavrilo Mumovic <gajo357@gmail.com>
2017-11-19 19:54:22 +0100

    Added Add Post method

--- FitnessFrog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ FitnessFrog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -42,6 +42,13 @@ namespace Treehouse.FitnessFrog.Controllers
             return View();
         }
 
+        [ActionName(nameof(Add))]
+        [HttpPost]
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+
         public ActionResult Edit(int? id)
         {
             if (!id.HasValue)
