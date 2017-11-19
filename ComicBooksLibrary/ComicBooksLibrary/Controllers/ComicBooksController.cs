9d1c9aefe9d4241d5fb663867f661129f756eac2
Gavrilo Mumovic <gajo357@gmail.com>
2017-11-19 11:26:31 +0100

    Initial solution and ComicBooks controller

---
+++ ComicBooksLibrary/ComicBooksLibrary/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ComicBooksLibrary.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public string Details()
+        {
+            return "Hello from Details ASP.NET";
+        }
+    }
+}
