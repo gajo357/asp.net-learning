0694ade1612609820685b74ce619bb120f97ad27
Gavrilo Mumovic <gajo357@gmail.com>
2017-11-19 12:58:43 +0100

    Added Detail Razor View

--- ComicBooksLibrary/ComicBooksLibrary/Controllers/ComicBooksController.cs
+++ ComicBooksLibrary/ComicBooksLibrary/Controllers/ComicBooksController.cs
@@ -5,14 +5,9 @@ namespace ComicBooksLibrary.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Details()
+        public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from Details ASP.NET");
+            return View();
         }
     }
 }
