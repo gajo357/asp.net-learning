51c9400c71c6296b614c2d0652acd96880c108da
Gavrilo Mumovic <gajo357@gmail.com>
2017-11-19 14:29:16 +0100

    Updated ComicBook model

--- ComicBooksLibrary/ComicBooksLibrary/Models/ComicBook.cs
+++ ComicBooksLibrary/ComicBooksLibrary/Models/ComicBook.cs
@@ -8,5 +8,22 @@
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DesplayText
+        {
+            get
+            {
+                return $"{SeriesTitle} #{IssueNumber}";
+            }
+        }
+
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return $"{SeriesTitle.Replace(" ", "-").ToLower()}-{IssueNumber}.jpg";
+            }
+        }
     }
 }
