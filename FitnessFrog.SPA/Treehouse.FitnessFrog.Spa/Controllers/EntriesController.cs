2d7c2ee7981525adde15b2deb215da18a6bae171
Gavrilo Mumovic <gajo357@gmail.com>
2017-11-25 10:00:47 +0100

    Updated EntriesController to use the repository

--- FitnessFrog.SPA/Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
+++ FitnessFrog.SPA/Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
@@ -20,36 +20,31 @@ namespace Treehouse.FitnessFrog.Spa.Controllers
         [HttpGet]
         public IEnumerable<Entry> Get()
         {
-            var acitvityBiking = new Activity { Name = "Biking" };
-            return new List<Entry>
-            {
-                new Entry(2017, 1, 2, acitvityBiking, 10.0m),
-                new Entry(2017, 1, 3, acitvityBiking, 12.2m)
-            };
+            return _entriesRepository.GetList();
         }
 
         [HttpGet]
         public Entry Get(int id)
         {
-            return null;
+            return _entriesRepository.Get(id);
         }
 
         [HttpPost]
         public void Post(Entry entry)
         {
-             
+            _entriesRepository.Add(entry);             
         }
 
         [HttpPut]
         public void Put(int id, Entry entry)
         {
-
+            _entriesRepository.Update(entry);
         }
 
         [HttpDelete]
         public void Delete(int id)
         {
-
+            _entriesRepository.Delete(id);
         }
     }
 
