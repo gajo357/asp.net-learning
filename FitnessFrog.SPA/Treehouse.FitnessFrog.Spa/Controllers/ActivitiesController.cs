d579efc199526340ce03937e08887d3bae099ce9
Gavrilo Mumovic <gajo357@gmail.com>
2017-11-25 10:56:45 +0100

    Added Intensities and Activities controllers

---
+++ FitnessFrog.SPA/Treehouse.FitnessFrog.Spa/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Data;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class ActivitiesController : ApiController
+    {
+        private readonly ActivitiesRepository _activitiesRepository;
+
+        public ActivitiesController(ActivitiesRepository activitiesRepository)
+        {
+            _activitiesRepository = activitiesRepository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_activitiesRepository.GetList());
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var activity = _activitiesRepository.Get(id);
+            if (activity == null)
+                return NotFound();
+
+            return Ok(activity);
+        }
+    }
+}
---
+++ FitnessFrog.SPA/Treehouse.FitnessFrog.Spa/Controllers/IntensitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using static Treehouse.FitnessFrog.Shared.Models.Entry;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class IntensitiesController : ApiController
+    {
+        public IntensitiesController()
+        {
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(
+                Enum.GetValues(typeof(IntensityLevel))
+                .Cast<IntensityLevel>()
+                .Select(s => new { id = (int)s, name = s.ToString()})
+                .ToList()
+                );
+        }
+    }
+}
